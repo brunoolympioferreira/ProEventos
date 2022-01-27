e85da8182dee04020ba23f2d693a3a3fee6a7281
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2022-01-27 09:43:33 -0300

    Layout Responsivo + Forms

--- Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
@@ -12,73 +12,7 @@ import { Evento } from '../../models/Evento';
   // providers: [EventoService]
 })
 export class EventosComponent implements OnInit {
+  ngOnInit(): void {
 
-  modalRef = {} as BsModalRef;
-  public eventos: Evento[] = [];
-  public eventosFiltrados: Evento[] = [];
-  public larguraImagem = 150;
-  public margemImagem = 2;
-  public exibirImagem: boolean = true;
-  private filtrolistado = '';
-
-  public get filtroLista(): string {
-    return this.filtrolistado;
   }
-
-  public set filtroLista(value: string){
-    this.filtrolistado = value;
-    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
-  }
-
-  public filtrarEventos(filtrarPor: string) : Evento[] {
-    filtrarPor = filtrarPor.toLocaleLowerCase();
-    return this.eventos.filter(
-      (evento: { tema: string; local: string }) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
-      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
-    );
-  }
-
-  constructor(
-    private eventoService:EventoService,
-    private modalService: BsModalService,
-    private toastr: ToastrService,
-    private spinner: NgxSpinnerService,
-) {}
-
-  public ngOnInit(): void {
-    this.spinner.show();
-    this.getEventos();
-  }
-
-  public alterarImagem(): void{
-    this.exibirImagem = !this.exibirImagem;
-  }
-
-  public getEventos(): void {
-    this.eventoService.getEventos().subscribe({
-      next: (eventos: Evento[]) => {
-        this.eventos = eventos;
-        this.eventosFiltrados = this.eventos;
-      },
-      error:(error:any) => {
-        this.spinner.hide();
-        this.toastr.error('Erro ao carregar os eventos','Erro!')
-      },
-      complete: () =>this.spinner.hide()
-    });
-  }
-
-  openModal(template: TemplateRef<any>) {
-    this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
-  }
-
-  confirm(): void {
-    this.modalRef?.hide();
-    this.toastr.success('O Evento foi deletado com sucesso','Deletado');
-  }
-
-  decline(): void {
-    this.modalRef?.hide();
-  }
-
 }
